f288ef521d553e47ed2072fd65b14ddc9b8408cd
velikoborecami <velikoborecami@oblgas.by>
2021-04-12 15:12:30 +0300

    Configured main_activity.xml

--- app/src/main/java/com/humansuit/watchme/ui/MainActivity.kt
+++ app/src/main/java/com/humansuit/watchme/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.humansuit.watchme.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.humansuit.watchme.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val appToolBar = findViewById<Toolbar>(R.id.appToolBar)
+        setSupportActionBar(appToolBar)
 
     }
 
